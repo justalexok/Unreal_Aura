78e9b604c8ac9768aaf57ddd9009d835963c45bd
justalexok <76539791+justalexok@users.noreply.github.com>
2023-11-18 23:25:14 +0000

    After Lesson 164

---
+++ Source/Aura/Private/AI/AuraAIController.cpp
@@ -0,0 +1,15 @@
+// AGoulder Copyright
+
+
+#include "AI/AuraAIController.h"
+
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+AAuraAIController::AAuraAIController()
+{
+	Blackboard= CreateDefaultSubobject<UBlackboardComponent>("BlackboardComponent");
+	check(Blackboard);
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTreeComponent");
+	check(BehaviorTreeComponent);
+}
