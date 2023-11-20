440deb2cf9bf0d065ce4eaeff396288557867c2c
justalexok <76539791+justalexok@users.noreply.github.com>
2023-11-20 09:59:03 +0000

    After Lesson 171

---
+++ Source/Aura/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,9 @@
+// AGoulder Copyright
+
+
+#include "AI/BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
