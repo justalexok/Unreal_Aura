c45b7f1f71b1e670ae68e24329547d822532b587
justalexok <76539791+justalexok@users.noreply.github.com>
2023-11-30 12:28:54 +0000

    After Lesson 251

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -14,6 +14,7 @@ class UAttributeSet;
 /**
  * 
  */
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32 /*StatValue*/)
 UCLASS()
 class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
 {
@@ -28,6 +29,14 @@ public:
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
 	FORCEINLINE int32 GetPlayerLevel() const  { return Level; }
+	FORCEINLINE int32 GetXP() const  { return XP; }
+	
+	void SetLevel(int32 InLevel);
+	void AddToLevel(int32 InLevel);
+
+	void SetXP(int32 InXP);
+	void AddToXP(int32 InXP);
+
 protected:
 
 	UPROPERTY(VisibleAnywhere)
@@ -36,11 +45,19 @@ protected:
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
 
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
 private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_Level)
 	int32 Level = 1;
 
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_XP)
+	int32 XP = 0;
+
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+	
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 	
 };
